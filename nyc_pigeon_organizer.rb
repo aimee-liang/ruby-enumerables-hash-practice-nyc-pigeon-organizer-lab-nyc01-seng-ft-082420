bc2188317bc963a2c5fbd8288f86f40480805d22
Aimee Liang <aimeeeliang@gmail.com>
2020-08-13 00:53:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   pigeon_info = {}
   data.each do |color_gender_lives, value|
     value.each do |all_names, stats|
-      all_names.each do |names|
+      all_names.each do |name|
         if pigeon_info[name] == nil 
           pigeon_info[name] = {}
         end
